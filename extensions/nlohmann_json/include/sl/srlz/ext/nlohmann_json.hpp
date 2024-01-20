5e31700058d1d952cc3b64557104383d9268de38
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-01-20 05:08:22 +0100

    simplify extension to<nlohmann::json>

--- extensions/nlohmann_json/include/sl/srlz/ext/nlohmann_json.hpp
+++ extensions/nlohmann_json/include/sl/srlz/ext/nlohmann_json.hpp
@@ -5,39 +5,17 @@
 
 namespace sl::srlz {
 
-template <typename CharT>
-struct impl::to<nlohmann::json, std::basic_string<CharT>> {
-    template <typename... Args>
-    nlohmann::json operator()(const std::basic_string<CharT>& value, Args&&...) const {
-        return value;
-    }
-};
-
-template <typename CharT>
-struct impl::to<nlohmann::json, std::basic_string_view<CharT>> {
-    template <typename... Args>
-    nlohmann::json operator()(const std::basic_string_view<CharT>& value, Args&&...) const {
-        return value;
-    }
-};
-
 template <>
 struct impl::init_to<nlohmann::json> {
-    template <typename T, typename... Args>
-    nlohmann::json operator()(const T&, Args&&...) const {
-        return nlohmann::json{};
-    }
-
-    template <typename T, typename... Args>
-        requires(meta::is_range_v<T> && !meta::is_mapping_v<T>)
-    nlohmann::json operator()(const T&, Args&&...) const {
-        return nlohmann::json::array();
-    }
-
-    template <typename T, typename... Args>
-        requires meta::is_mapping_v<T>
-    nlohmann::json operator()(const T&, Args&&...) const {
-        return nlohmann::json::object();
+    template <typename SrcT, typename... Args>
+    nlohmann::json operator()(const SrcT&, Args&&...) const {
+        if constexpr (meta::is_mapping_v<SrcT>) {
+            return nlohmann::json::object();
+        } else if constexpr (meta::is_range_v<SrcT>) {
+            return nlohmann::json::array();
+        } else {
+            return nlohmann::json{};
+        }
     }
 };
 
